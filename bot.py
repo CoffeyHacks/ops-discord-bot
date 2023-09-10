bc2628b3c7ea0ebe2b6aa90061a2799af20d74ad
Steve Coffey <111541690+CoffeyHacks@users.noreply.github.com>
2023-09-10 23:16:15 +0000

    It might be working

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import json
 import discord
 from discord.ext import commands, tasks
+from discord_slash import SlashCommand
 import requests
 from dotenv import load_dotenv
 
@@ -13,11 +14,10 @@ OPS_PASSWORD = os.getenv("OPS_PASSWORD")
 OPS_TOKEN = os.getenv("OPS_TOKEN")
 
 intents = discord.Intents.all()
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
 
 def get_new_ops_token():
-    """Retrieve a new authentication token from OPS."""
     auth_data = {
         'username': OPS_USERNAME,
         'password': OPS_PASSWORD
@@ -32,36 +32,22 @@ def get_new_ops_token():
         return None
 
 def get_ops_endpoint(data):
-    if 'phoneNumber' in data:
+    if 'phonenumber' in data:
         return "https://api.openpeoplesearch.com/api/v1/Consumer/PhoneSearch"
-    elif 'emailAddress' in data:
+    elif 'emailaddress' in data:
         return "https://api.openpeoplesearch.com/api/v1/Consumer/EmailAddressSearch"
-    elif 'firstName' in data and 'lastName' in data:
-        if 'address' in data and 'dob' in data:
-            return "https://api.openpeoplesearch.com/api/v1/Consumer/NameDOBAddressSearch"
-        elif 'address' in data:
-            return "https://api.openpeoplesearch.com/api/v1/Consumer/NameAddressSearch"
-        elif 'dob' in data:
+    elif 'address' in data:
+        return "https://api.openpeoplesearch.com/api/v1/Consumer/AddressSearch"
+    elif 'firstname' in data and 'lastname' in data:
+        if 'dob' in data:
             return "https://api.openpeoplesearch.com/api/v1/Consumer/NameDOBSearch"
         else:
             return "https://api.openpeoplesearch.com/api/v1/Consumer/NameSearch"
-    elif 'businessName' in data:
-        return "https://api.openpeoplesearch.com/api/v1/Consumer/BusinessSearch"
-    elif 'address' in data:
-        return "https://api.openpeoplesearch.com/api/v1/Consumer/AddressSearch"
-    elif 'poBox' in data:
-        return "https://api.openpeoplesearch.com/api/v1/Consumer/PoBoxSearch"
     else:
         return None
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    renew_ops_token.start()  # Start the token renewal loop
-
 @tasks.loop(hours=6.5)
 async def renew_ops_token():
-    """Renew the OPS token at regular intervals."""
     global OPS_TOKEN
     new_token = get_new_ops_token()
     if new_token:
@@ -70,27 +56,62 @@ async def renew_ops_token():
     else:
         print("Failed to renew the OPS token. Trying again in 6.5 hours.")
 
-@bot.command(name='search', help='Use this command with the necessary parameters to perform a search.')
-async def search(ctx, firstname=None, lastname=None, middlename=None, dob=None, address=None, phoneNumber=None, emailAddress=None, businessName=None, poBox=None):
-    """Search for data in OPS using the provided parameters."""
+@slash.slash(
+    name="search",
+    description="Perform a search with the given parameters.",
+    options=[
+        {
+            "name": "firstname",
+            "description": "First Name of the person you're looking for.",
+            "type": 3,
+            "required": False
+        },
+        {
+            "name": "lastname",
+            "description": "Last Name of the person you're looking for.",
+            "type": 3,
+            "required": False
+        },
+        {
+            "name": "dob",
+            "description": "Date of Birth of the person you're looking for (in MMDDYYYY format).",
+            "type": 3,
+            "required": False
+        },
+        {
+            "name": "phonenumber",
+            "description": "Phone number of the person you're looking for.",
+            "type": 3,
+            "required": False
+        },
+        {
+            "name": "emailaddress",
+            "description": "Email address of the person you're looking for.",
+            "type": 3,
+            "required": False
+        },
+        {
+            "name": "address",
+            "description": "Address of the person you're looking for.",
+            "type": 3,
+            "required": False
+        }
+    ]
+)
+async def _search(ctx, firstname=None, lastname=None, dob=None, phonenumber=None, emailaddress=None, address=None):
     data = {
-        "firstName": firstname,
-        "lastName": lastname,
-        "middleName": middlename,
+        "firstname": firstname,
+        "lastname": lastname,
         "dob": dob,
-        "address": address,
-        "phoneNumber": phoneNumber,
-        "emailAddress": emailAddress,
-        "businessName": businessName,
-        "poBox": poBox
+        "phonenumber": phonenumber,
+        "emailaddress": emailaddress,
+        "address": address
     }
-
-    # Remove None values to refine the search payload
     data = {k: v for k, v in data.items() if v}
-
+    
     endpoint_url = get_ops_endpoint(data)
     if not endpoint_url:
-        await ctx.send("Invalid search parameters. Please check your query and try again.")
+        await ctx.send(content="Invalid search parameters. Please check your query and try again.")
         return
 
     headers = {
@@ -98,28 +119,18 @@ async def search(ctx, firstname=None, lastname=None, middlename=None, dob=None, 
         'Content-Type': 'application/json',
     }
 
-    try:
-        response = requests.post(endpoint_url, headers=headers, json=data, timeout=120)
-        response.raise_for_status()  # This will raise an HTTPError for bad responses
-    except requests.exceptions.RequestException as e:
-        await ctx.send(f"An error occurred while making the request: {e}")
-        return
+    response = requests.post(endpoint_url, headers=headers, json=data, timeout=120)
 
-    # Check if the response content type is JSON
-    content_type = response.headers.get("Content-Type", "")
-    if "application/json" not in content_type:
-        await ctx.send("Received an unexpected response from the server. Please try again later.")
-        return
-
-    try:
-        results = response.json()
-    except json.JSONDecodeError:
-        await ctx.send("Error occurred while processing server response. Please try again later.")
-        return
-
-    if response.status_code == 200 and 'data' in results:
-        await ctx.send(str(results['data']))
+    if response.status_code == 200:
+        try:
+            results = response.json()
+            if 'data' in results:
+                await ctx.send(content=str(results['data']))
+            else:
+                await ctx.send(content="No data found.")
+        except json.JSONDecodeError:
+            await ctx.send(content="Error occurred while processing server response. Please try again later.")
     else:
-        await ctx.send("Error occurred while searching. Please try again.")
+        await ctx.send(content=f"Error occurred while searching. Please try again. (Error: {response.status_code})")
 
 bot.run(DISCORD_TOKEN)
